644a9013d3923b8be88c64fc7e562fef30e38af8
miorlan2 <adduser@Alfie.localdomain>
2024-02-24 09:51:48 +0300

    Add mymath

--- mymath.go
+++ mymath.go
@@ -14,3 +14,13 @@ func Sqrt(x float64) float64 {
 func Yn(n int, y float64) float64 {
 	return math.Yn(n, y)
 }
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
